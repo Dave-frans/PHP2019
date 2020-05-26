e4374e820d4871af05f7c973c8512064d110b0e2
Daviangelo Frans <djrfrans@st.noorderpoort.nl>
2020-05-26 13:04:00 +0200

    add H03 opdracht04

---
+++ H03/opdracht04.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+
+	$x = 35;
+while($x >= 25) {
+echo "hoppelepee";
+$x--;
+}
+
+echo "<h1>Hier begint de for loop</h1>";
+
+for ($i = 35 ; $i >= 25 ; $i--){
+    echo "Hoppelepee";
+}
+?>
+</body>
+</html>
