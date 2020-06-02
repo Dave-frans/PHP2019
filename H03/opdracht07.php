d8751e02a37b86d17bd49c8f35c302610c5a6bee
Daviangelo Frans <djrfrans@st.noorderpoort.nl>
2020-06-02 14:05:05 +0200

    add H03 opdracht07

---
+++ H03/opdracht07.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<style>
+    table, tr, td {
+        border: solid 1px white;
+        border-collapse: collapse;
+    }
+    td {
+        padding: 10px;
+    }
+    td.border {
+        border: solid 1px grey;
+    }
+    body {
+        font-family: sans-serif;
+    }
+
+</style>
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<table>
+    <tbody>
+    <tr><td class="border">De spartelkuikens</td>
+        <td class="border">25</td>
+        <td><img src="//unsplash.it/30" alt="Zwemmer">
+            <img src="//unsplash.it/30" alt="Zwemmer">
+            <img src="//unsplash.it/30" alt="Zwemmer">
+            <img src="//unsplash.it/30" alt="Zwemmer">
+            <img src="//unsplash.it/30" alt="Zwemmer"></td></tr>
+
+    <tr><td class="border">De waterbuffels</td>
+        <td class="border">32</td>
+        <td><img src="//unsplash.it/30" alt="Zwemmer">
+            <img src="//unsplash.it/30" alt="Zwemmer">
+            <img src="//unsplash.it/30" alt="Zwemmer">
+            <img src="//unsplash.it/30" alt="Zwemmer">
+            <img src="//unsplash.it/30" alt="Zwemmer">
+            <img src="//unsplash.it/30" alt="Zwemmer"></td></tr>
+
+    <tr><td class="border">Plonsmderin</td>
+        <td class="border">11</td>
+        <td><img src="//unsplash.it/30" alt="Zwemmer">
+            <img src="//unsplash.it/30" alt="Zwemmer"></td></tr>
+
+    <tr><td class="border">Bommetje</td>
+        <td class="border">23</td>
+        <td><img src="//unsplash.it/30" alt="Zwemmer">
+            <img src="//unsplash.it/30" alt="Zwemmer">
+            <img src="//unsplash.it/30" alt="Zwemmer">
+            <img src="//unsplash.it/30" alt="Zwemmer"></td></tr>
+    </tbody>
+</table>
+</body>
+</html>
+
