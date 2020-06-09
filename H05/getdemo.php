74682cde2505c5fa5f9c6489a1a8e8dc6e3874aa
Daviangelo Frans <djrfrans@st.noorderpoort.nl>
2020-06-09 13:34:43 +0200

    .

---
+++ H05/getdemo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<h1>Get demo</h1>
+<form action="resultaat.php" method="get">
+    <input type="text" name="voornaam">
+    <input type="text" name="achternaam">
+    <input type="submit" name="GO!">
+</form>
+</body>
+</html>
