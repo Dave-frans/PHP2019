de00db2d1738ceaa2151d7d23351753859803147
Daviangelo Frans <djrfrans@st.noorderpoort.nl>
2020-09-22 12:30:23 +0200

    H5 opdracht 5

--- H05/opdracht05.php
+++ H05/opdracht05.php
@@ -21,6 +21,26 @@ function check_credentials($mailadres, $wachtwoord) {
     return false;
 }
 
+if (isset($_POST['knop'])) {
+    if (!isset($_POST['$mailadres']) OR empty($_POST['$mailadres'])) {
+        $_SESSION['error'] = "Je bent vergeten je mailadres in te vullen.";
+        header("Location:" . $_SERVER['PHP_SELF']);
+    }
+    if (!isset($_POST['$wachtwoord']) OR empty($_POST['$wachtwoord'])) {
+        $_SESSION['error'] = "Je bent vergeten je wachtwoord in te vullen.";
+        header("Location:" . $_SERVER['PHP_SELF']);
+    }
+    $mailadres = $_POST['$mailadres'];
+    $wachtwoord = $_POST ['$wachtwoord'];
+
+    if (check_credentials($mailadres, $wachtwoord)) {
+        exit("Welkom!");
+    }else{
+        exit("Sorry! Geen toegang...");
+    }
+
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -33,19 +53,20 @@ function check_credentials($mailadres, $wachtwoord) {
 
 <?php if (!isset($_POST['knop'])) { ?>
 
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>"method="post">
-    Naam: <input type="text" name="mailadres" placeholder="mailadres"><br>
-    Wachtwoord: <input type="password" name="wachtwoord" placeholder="wachtwoord">
-    <input type="submit" name="knop" value="VERSTUUR">
-</form>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        Naam: <input type="text" name="mailadres" placeholder="mailadres"><br>
+        Wachtwoord: <input type="password" name="wachtwoord" placeholder="wachtwoord">
+        <input type="submit" name="knop" value="VERSTUUR">
+    </form>
 
-<?php
-if (isset($_SESSION['error']) AND !empty($_SESSION['error'])) {
-    echo '<div class="error">' . $_SESSION['error'] . "</div>";
-    $_SESSION['error'] = null;
-}
-?>
-<?php } ?>
+    <?php
+    if (isset($_SESSION['error']) AND !empty($_SESSION['error'])) {
+        echo '<div class="error">' . $_SESSION['error'] . "</div>";
+        $_SESSION['error'] = null;
+    }
+    ?>
+
+    <?php } ?>
 
 
 
