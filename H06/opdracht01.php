31809c2efc8358006bd7926393923d310c1cf834
Daviangelo Frans <djrfrans@st.noorderpoort.nl>
2020-10-05 15:49:46 +0200

    Add H06 Opdracht01 and Opdracht02 Moved files from H06 to H07

--- H06/opdracht01.php
+++ H06/opdracht01.php
@@ -2,17 +2,52 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>php opdracht</title>
+    <title>link</title>
+    <style>
+        table, tr, td{
+            border: solid 1px black;
+            border-collapse: collapse;
+            padding: 10px;
+        }
+    </style>
 </head>
 <body>
-<form action="Login.php"method="post">
-    <label for="email">Login:</label>
-    <input type="text" name="email"><br>
-    <label for="password">Password:</label>
-    <input type="password" name="wachtwoord"><br>
-    <input type="submit" name="knop">
-</form>
+<?php
+// inloggegevens sql database en op welke sever je zit
+$servername = "localhost";
+$username = "root";
+$password = "";
 
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=newschema", $username, $password);
+    //set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//    echo "connected successfully";
+}
+catch (PDOException $e)
+{
+    echo "connection failed:" . $e->getMessage() . '<br>';
+}
+
+
+$query = "SELECT * FROM cursist;";
+$stmt = $conn->prepare($query) or die('Error 34');
+$stmt->execute() or die('Error 35');
+echo '<table>';
+while ($array = $stmt->fetch()) {
+    echo '<tr>';
+    echo "<td>" . $array['cursistnr'] . "</td>";
+    echo "<td>" . $array['naam'] . "</td>";
+    echo "<td>" . $array['roepnaam'] . "</td>";
+    echo "<td>" . $array['straat'] . "</td>";
+    echo "<td>" . $array['postcode'] . "</td>";
+    echo "<td>" . $array['plaats'] . "</td>";
+    echo "<td>" . $array['geslacht'] . "</td>";
+    echo "<td>" . $array['geb_datum'] . "</td>";
+    echo '</tr>';
+}
+echo '</tables>';
+?>
 
 </body>
 </html>
